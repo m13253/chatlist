3850c5e296954ff37277fe0e3cba87d0742ae35d
Star Brilliant <m13253@hotmail.com>
2012-09-09 17:03:38 +0800

    Filter orzbot

--- msgfilters.py
+++ msgfilters.py
@@ -13,12 +13,16 @@ re_autoreply = re.compile('[aA]uto ?[rR]eply[:：\\]]|自动回复[:：\\]]|我(
 re_ayt = re.compile('^([aA]ny ?(body|one) there|这?群?里?[边面]?有人在?[吗么马]).{,5}$')
 
 def filter_autoreply(xmpp, msg):
+    if(msg['from'].bare=='orzbot@erhandsome.org'):
+        return True
     if re_autoreply.match(msg['body']):
         msg.reply(misc.replace_prefix(_('It seems that you are using auto reply or a plugin that automatically sends messages, please disable this function in order not to disturb other users in this group.\nIf you are sure that previous message is sent by you, please put /-say before your message and send your previous message again.\nThe last message you sent is:\n%s') % msg['body'], config.command_prefix[0])).send()
         return False
     return True
 
 def filter_ayt(xmpp, msg):
+    if(msg['from'].bare=='orzbot@erhandsome.org'):
+        return True
     if re_ayt.match(msg['body']):
         msg.reply(misc.replace_prefix(_('Please type /-ls to list online users, or type /-help for further help.'), config.command_prefix[0])).send()
     return True
