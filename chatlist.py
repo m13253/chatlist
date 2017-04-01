a228e9b184afac8280efaac1a9cac7338fe1bc63
Tom Li <biergaizi@member.fsf.org>
2017-04-02 00:23:34 +0800

    new config: omit nicknames in messages from specific JIDs
    This commit add a new option, "omit_nickname_prefix", which is
    a list of JIDs of whom their nickname prefixes are omited in
    their messages.
    
    It is useful for bots and other automated messages that their
    nicknames should be hidden.
    
    Signed-off-by: Tom Li <biergaizi@member.fsf.org>

--- chatlist.py
+++ chatlist.py
@@ -139,7 +139,10 @@ class XMPPBot(sleekxmpp.ClientXMPP):
             logging.warning('Exception: %s: %s\n' % (type(e).__name__, e))
 
     def dispatch_message(self, from_jid, body):
-        self.send_except(from_jid, '%s: %s' % (misc.getnick(self, from_jid), body))
+        if from_jid in config.omit_nickname_prefix:
+            self.send_except(from_jid, body)
+        else:
+            self.send_except(from_jid, '%s: %s' % (misc.getnick(self, from_jid), body))
 
     def send_except(self, except_jid, body):
         nowtime=time.time()
